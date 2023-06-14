0b27c44a1c180f8d996a59bee6cf466dfb71b501
myway00 <junior0614@naver.com>
2023-06-15 02:55:42 +0900

    #2 ADD : Http11Processor 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -3,18 +3,23 @@ package org.apache.coyote.http11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import nextstep.jwp.db.InMemoryUserRepository;
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
+import org.apache.coyote.http11.message.common.ContentType;
+import org.apache.coyote.http11.message.request.HttpRequest;
+import org.apache.coyote.http11.message.request.RequestUri;
+import org.apache.coyote.http11.message.response.HttpResponse;
+import org.apache.coyote.http11.util.StaticFileUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Http11Processor implements Runnable, Processor {
 
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
+    private static final String NEW_LINE = "\r\n";
 
     private final Socket connection;
 
@@ -27,33 +32,70 @@ public class Http11Processor implements Runnable, Processor {
         process(connection);
     }
 
+    /**
+     * Connector 로부터 받은 Socket 의 InputStream 을 읽고 데이터를 처리한 후
+     * OutputStream 에 데이터를 담아 클라이언트에게 전달
+     */
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var bufferReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            HttpRequest httpRequest = HttpRequest.parse(readHttpRequest(bufferReader));
+            RequestUri requestUri = httpRequest.getRequestUri();
 
-            String httpMessageStartLine = bufferedReader.readLine();
-            String requestUri = httpMessageStartLine.split(" ")[1];
+            if (requestUri.hasExtension()) { // 정적 파일 서빙
+                HttpResponse httpResponse = new HttpResponse.Builder()
+                        .contentType(requestUri.getExtension())
+                        .body(StaticFileUtil.readFile(requestUri.getPath()))
+                        .build();
 
-            URL fileUri = getClass().getClassLoader().getResource("static" + requestUri);
-            Path path = Path.of(fileUri.getPath());
-            String fileExtension = requestUri.substring(requestUri.lastIndexOf(".") + 1);
-            final var responseBody = Files.readString(path);
+                writeHttpResponse(outputStream, httpResponse);
+            }
 
-            final String response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/" + fileExtension + ";charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
+            if (requestUri.matches("/")) {
+                HttpResponse httpResponse = new HttpResponse.Builder()
+                        .contentType(ContentType.HTML)
+                        .body("Hello world!")
+                        .build();
 
-            outputStream.write(response.getBytes());
-            outputStream.flush();
+                writeHttpResponse(outputStream, httpResponse);
+            }
+
+            if (requestUri.matches("/login")) {
+                String account = requestUri.getQuery("account").orElse("");
+                String password = requestUri.getQuery("password").orElse("");
+
+                InMemoryUserRepository.findByAccount(account)
+                        .filter(user -> user.checkPassword(password))
+                        .ifPresentOrElse(System.out::println, () -> System.out.println("유저가 없습니다."));
+
+                HttpResponse httpResponse = new HttpResponse.Builder()
+                        .contentType(ContentType.HTML)
+                        .body(StaticFileUtil.readFile("/login.html"))
+                        .build();
+
+                writeHttpResponse(outputStream, httpResponse);
+            }
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String readHttpRequest(final BufferedReader bufferedReader) throws IOException {
+        StringBuilder httpRequest = new StringBuilder();
+        while (bufferedReader.ready()) {
+            String line = bufferedReader.readLine();
+            httpRequest.append(line).append(NEW_LINE);
+        }
+
+        return httpRequest.toString();
+    }
+
+    private void writeHttpResponse(final OutputStream outputStream, final HttpResponse httpResponse)
+            throws IOException {
+        outputStream.write(httpResponse.generateMessage().getBytes());
+        outputStream.flush();
     }
 }
