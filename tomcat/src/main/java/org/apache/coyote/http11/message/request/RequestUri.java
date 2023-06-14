c3833160ccbed9909f29eb9469e669c4662b2aad
myway00 <junior0614@naver.com>
2023-06-15 00:13:08 +0900

    #2 ADD : HTTP Request 파싱 기능 (start line , uri, msg)

---
+++ tomcat/src/main/java/org/apache/coyote/http11/message/request/RequestUri.java
@@ -0,0 +1,51 @@
+package org.apache.coyote.http11.message.request;
+
+import java.net.URI;
+import java.util.Optional;
+
+public class RequestUri {
+
+    private static final String SCHEME_HOST_PORT_REGEX = "http?://.*/";
+    private static final String QUERY_DELIMITER = "?";
+
+    private final URI uri;
+
+    public RequestUri(final String url) {
+        this.uri = URI.create(removeSchemeHostPort(url));
+    }
+
+    private String removeSchemeHostPort(final String url) {
+        return url.replaceAll(SCHEME_HOST_PORT_REGEX, "");
+    }
+
+    public Optional<String> getQuery(final String key) {
+        String query = uri.getQuery();
+        QueryString queryString = new QueryString(query);
+        return queryString.getQuery(key);
+    }
+
+    public String getPathWithoutQuery() {
+        return uri.getPath().split("\\?")[0];
+    }
+
+    public String getExtension() {
+        String pathWithoutQuery = getPathWithoutQuery();
+        int lastIndexOfSlash = pathWithoutQuery.lastIndexOf("/");
+        String filename = pathWithoutQuery.substring(lastIndexOfSlash);
+
+        int lastDotIndex = filename.lastIndexOf(".");
+        if (lastDotIndex < 0) {
+            return "";
+        }
+
+        return filename.substring(lastDotIndex + 1);
+    }
+
+    public boolean hasExtension() {
+        return !getExtension().isBlank();
+    }
+
+    public boolean matches(final String uri) {
+        return this.uri.getPath().equals(uri);
+    }
+}
