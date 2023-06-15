735f109a3e6f8da038481c275fcdb1dca1962f4f
myway00 <junior0614@naver.com>
2023-06-15 12:26:05 +0900

    #3, #8 ADD : Thread pool 갯수, 동시성 제어 자료구조 추가

--- tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
@@ -1,15 +1,16 @@
 package org.apache.coyote.http11.session;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.ToString;
 
 @ToString
 public class SessionManager {
 
-    private static final Map<String, Session> SESSIONS = new HashMap<>();
+    // ConcurrentHashMap : 동시 접속 시 멀티쓰레드 환경에서 경쟁상태가 발생 -> 세션 데이터의 정합성 이슈 방지
+    private static final Map<String, Session> SESSIONS = new ConcurrentHashMap<>();
 
     private SessionManager() {
     }
