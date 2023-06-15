4b0ada4d8176e0c675ea6794e4a919122c0ddbc0
myway00 <junior0614@naver.com>
2023-06-15 10:44:11 +0900

    #2 ADD : Request method & uri 일치 여부 check method

--- tomcat/src/main/java/org/apache/coyote/http11/message/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/message/request/HttpRequest.java
@@ -3,6 +3,7 @@ package org.apache.coyote.http11.message.request;
 import lombok.Getter;
 import lombok.ToString;
 import org.apache.coyote.http11.message.common.HttpHeaders;
+import org.apache.coyote.http11.message.common.HttpMethod;
 
 @ToString
 @Getter
@@ -50,6 +51,19 @@ public class HttpRequest {
     private static String parseBody(final String message) {
         int bodyStartIndex = message.indexOf(BODY_SEPARATOR);
         return message.substring(bodyStartIndex);
+    }
+
+    /**
+     * 요청 메소드와 URI 가 일치 여부 검사하는 메소드
+     * @param method
+     * @param uri
+     * @return
+     */
+    public boolean matches(final HttpMethod method, final String uri) {
+        boolean methodMatches = requestLine.getMethod().equals(method);
+        boolean uriMatches = requestLine.getRequestUri().matches(uri);
+
+        return methodMatches && uriMatches;
     }
 
     public RequestUri getRequestUri() {
